7745cc899685a80a6e226304fe5f268165dcf7e9
Quentin zang <quentinzang@gmail.com>
2019-09-19 21:28:07 +0800

    carthage support

---
+++ Example/Toast/Toast.h
@@ -0,0 +1,19 @@
+//
+//  Toast.h
+//  Toast
+//
+//  Created by ChuangLan on 2019/9/19.
+//  Copyright © 2019 Quentin123456. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Toast.
+FOUNDATION_EXPORT double ToastVersionNumber;
+
+//! Project version string for Toast.
+FOUNDATION_EXPORT const unsigned char ToastVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Toast/PublicHeader.h>
+
+
