49786d159d4cfb6fba0a0b711f265f25c25a2637
Quentin zang <quentinzang@gmail.com>
2019-09-20 09:29:00 +0800

    carthage Update

---
+++ QToast/QToast/QToast.h
@@ -0,0 +1,19 @@
+//
+//  QToast.h
+//  QToast
+//
+//  Created by ChuangLan on 2019/9/20.
+//  Copyright © 2019 QuentinZang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QToast.
+FOUNDATION_EXPORT double QToastVersionNumber;
+
+//! Project version string for QToast.
+FOUNDATION_EXPORT const unsigned char QToastVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QToast/PublicHeader.h>
+
+
